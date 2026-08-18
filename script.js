867a57a73ab643b22c456426346dfb869109a996
nidadayo <goonaoki0822@gmail.com>
2026-08-18 13:28:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -619,3 +619,55 @@ prefersDark.addEventListener(
   "change",
   updateTheme
 );
+
+const adminSecretButton =
+  document.getElementById(
+    "adminSecretButton"
+  );
+
+const adminLogin =
+  document.getElementById(
+    "adminLogin"
+  );
+
+
+const secretWord =
+  "nida-admin";
+
+
+if (
+  adminSecretButton &&
+  adminLogin
+) {
+
+  adminSecretButton.addEventListener(
+    "click",
+    () => {
+
+      const word =
+        prompt(
+          "秘密ワードを入力してください"
+        );
+
+
+      if (
+        word === secretWord
+      ) {
+
+        adminLogin.style.display =
+          "block";
+
+      } else if (
+        word !== null
+      ) {
+
+        alert(
+          "秘密ワードが違います。"
+        );
+
+      }
+
+    }
+  );
+
+}
