5934f40b2af5159f5d1cc474aff64f3b2b700f54
nidadayo <goonaoki0822@gmail.com>
2026-08-18 13:20:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
 /* =================================
+   管理者秘密入口
+================================= */
+
+const adminSecretButton =
+  document.getElementById(
+    "adminSecretButton"
+  );
+
+const adminLogin =
+  document.getElementById(
+    "adminLogin"
+  );
+
+
+const secretWord =
+  "nida-admin";
+
+
+if (
+  adminSecretButton &&
+  adminLogin
+) {
+
+  adminSecretButton.addEventListener(
+    "click",
+    () => {
+
+      const word =
+        prompt(
+          "秘密ワードを入力してください"
+        );
+
+
+      if (
+        word === secretWord
+      ) {
+
+        adminLogin.style.display =
+          "block";
+
+      } else if (
+        word !== null
+      ) {
+
+        alert(
+          "秘密ワードが違います。"
+        );
+
+      }
+
+    }
+  );
+
+}
+/* =================================
    ハンバーガーメニュー
 ================================= */
 
@@ -619,59 +674,3 @@ prefersDark.addEventListener(
   "change",
   updateTheme
 );
-
-/* =================================
-   管理者秘密入口
-================================= */
-
-const adminSecretButton =
-  document.getElementById(
-    "adminSecretButton"
-  );
-
-const adminLogin =
-  document.getElementById(
-    "adminLogin"
-  );
-
-
-const secretWord =
-  "nida-admin";
-
-
-if (
-  adminSecretButton &&
-  adminLogin
-) {
-
-  adminSecretButton.addEventListener(
-    "click",
-    () => {
-
-      const word =
-        prompt(
-          "秘密ワードを入力してください"
-        );
-
-
-      if (
-        word === secretWord
-      ) {
-
-        adminLogin.style.display =
-          "block";
-
-      } else if (
-        word !== null
-      ) {
-
-        alert(
-          "秘密ワードが違います。"
-        );
-
-      }
-
-    }
-  );
-
-}
